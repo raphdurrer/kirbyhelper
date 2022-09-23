93573f23a38b3501d3e0c2c3497fbcbb73b499f2
Raphael <raphael@docono.io>
2022-09-23 10:01:19 +0200

    Updated default template

--- templates/default.php
+++ templates/default.php
@@ -7,7 +7,7 @@ use Kirby\Toolkit\A;
 <?php snippet('header'); ?>
 <main class="site-main">
     <?php foreach ($page->layout()->toLayouts() as $layout) : ?>
-        <div class="grid<?php e($layout->gridwidth() == '100%', '--full'); ?>">
+        <div class="grid<?php e($layout->gridwidth() == '100%', '--full'); ?>" style="<?php e($layout->blockStart()->isNotEmpty(), '--topMargin:' . $layout->blockStart() . ';') ?><?php e($layout->blockEnd()->isNotEmpty(), '--bottomMargin:' . $layout->blockEnd() . ';') ?>">
             <?php foreach ($layout->columns() as $column) : ?>
                 <div class="column" style="--gridColumns:<?= $column->span() ?>">
                     <?php foreach ($column->blocks() as $block) : ?>
