d3ce0cc5058348f07ff934bfdc32b79b0c7cb767
Raphael <raphael@docono.io>
2022-09-23 10:01:38 +0200

    Updated Header Snippet with metatags

--- snippets/header.php
+++ snippets/header.php
@@ -1,29 +1,23 @@
 <!DOCTYPE html>
 <html lang="<?php e($kirby->languages()->isNotEmpty(), $kirby->language(), 'de') ?>">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="manifest.json">
-    <title>
-        <?php
-        $seotitle = '';
-        if ($page->seotitle()->isNotEmpty()) {
-            $seotitle = $page->seotitle();
-        } elseif ($site->seotitle()->isNotEmpty()) {
-            $seotitle = $site->seotitle();
-        } else {
-            $seotitle = $page->title();
-        }
-        echo $seotitle;
-        ?>
-    </title>
-
-    <?php echo $page->metaTags(['og', 'twitter']) ?>
+    <title><?= $meta['title'] ?></title>
+    <?php foreach ($meta as $tag => $value) : ?>
+        <?php if ($tag != 'title' && !empty($value)) : ?>
+            <meta name="<?= $tag ?>" content="<?= $value ?>">
+        <?php endif; ?>
+    <?php endforeach; ?>
+    <?php foreach ($opengraph as $ogtag => $value) : ?>
+        <?php if (!empty($value)) : ?>
+            <meta property="<?= $ogtag ?>" content="<?= $value ?>">
+        <?php endif; ?>
+    <?php endforeach; ?>
     <?= css('/assets/css/website.css', 'screen') ?>
     <?= css('/site/plugins/kirbyhelper/assets/css/print.css', 'print') ?>
     <?= css('@auto') ?>
 </head>
-
 <body>
